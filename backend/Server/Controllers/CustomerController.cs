cea149ed58601d593cbb0c974e5285ba26256e96
ajh657 <33629266+ajh657@users.noreply.github.com>
2020-06-23 17:20:08 +0300

    Pohja front ja back endille

---
+++ backend/Server/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private APIDBContext dbContext;
+
+        public CustomerController(APIDBContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]
+        public string getNewUserJson()
+        {
+            return null;
+        }
+    }
+}
---
+++ backend/Server/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Customer
+    {
+        public int id;
+        public string name;
+        public Address address;
+    }
+
+    public class Address
+    {
+        public string streetAdress;
+        public string city;
+        public string state;
+        public string zip;
+    }
+}
