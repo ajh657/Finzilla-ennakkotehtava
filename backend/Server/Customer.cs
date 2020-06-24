5e55d0d20be5a14a90bd141fc32e239a82698098
ajh657 <33629266+ajh657@users.noreply.github.com>
2020-06-24 14:10:04 +0300

    Fix

---
+++ backend/Server/Customer.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server
+{
+    public class NewCustomers
+    {
+        public List<Customer> customers;
+    }
+    public class Customer
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("address")]
+        [ForeignKey("Address")]
+        [NotMapped]
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        [JsonProperty("streetAddress")]
+        public string StreetAddress { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("zip")]
+        public long Zip { get; set; }
+    }
+}
---
+++ frontend/src/Customer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+class Customer extends Component {
+
+}
+
+export default Customer;
